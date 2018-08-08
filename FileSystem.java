ba73a7a7261b630e3a440fd90dd9489e70a3b40e
Taylor Brady <taylorllippert@hotmail.com>
2018-08-08 15:39:12 -0700

    Update FileSystem.java

--- FileSystem.java
+++ FileSystem.java
@@ -3,13 +3,19 @@ import java.util.*;
 public class FileSystem {
     HashMap<String, INode> filetoINode;
     boolean[] usedStatus;
+    
+    //Creates File System Object
+    //Needs to initialize usedStatus to false
     public FileSystem (){
         filetoINOde = new HashMap<>(); // key: filename, value: iNode
         usedStatus = new boolean[100]; // false if unused. true if used
     }
     
+    //called for function DB
     public void deleteBlocks(String name, int bnum){
-    //Taylor Working here
-    
+        INode fnode = filetoINode.get(name);
+        for(int i = 0; i < bnum; i++)
+            usedStatus[fnode.remove()] = false;
     }
+    
 }
