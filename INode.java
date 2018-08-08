a539ff2c84703992f84941d20e1b4a92b43cf476
adjaditya <40774211+adjaditya@users.noreply.github.com>
2018-08-08 16:25:06 -0700

    Update INode.java

--- INode.java
+++ INode.java
@@ -4,7 +4,7 @@ public class INode {
 
     public Date aTime; // last access (update in print??)
     public Date cTime; // time created
-    public Date mTime; // time modifited (update in add block & remove block)
+    public Date mTime; // time modified (update in add block & remove block)
 
     public int blockCount; // amount of blocks used
     public int[] directBlocks; // set size as 12. keeps track of the blocks used for this iNode
