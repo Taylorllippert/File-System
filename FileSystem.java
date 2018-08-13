6d7e18dc3287cc220f2e4eb619de571e92bf261d
adjaditya <40774211+adjaditya@users.noreply.github.com>
2018-08-13 14:52:01 -0700

    added a few comments

--- FileSystem.java
+++ FileSystem.java
@@ -18,28 +18,31 @@ public class FileSystem {
     	addBlocks(name, bnum);
     }
     
-    //called for function DB
+    // called for function DB
+    // remove a certain number of blocks in the INode of a certain file
     public void deleteBlocks(String name, int bnum){
         INode fnode = filetoINode.get(name);
         for(int i = 0; i < bnum; i++)
             dataBlocks[fnode.removeBlock()] = false;
     }
     
+    // add a certain number of blocks into the INode of a certain file
     public void addBlocks(String fileName, int blockNum) {
         INode n = filetoINode.get(fileName); // iNode target
 
+	// iterate through every blocks that exists in the file system
         for (int i = 0; i < dataBlocks.length; i++) {
-            if (dataBlocks[i] == false) { // insert to iNode
+            if (dataBlocks[i] == false) { // insert to iNode if block is not used
                 n.insertBlock(i);
                 blockNum--;
             }
-            if (blockNum == 0) {
+            if (blockNum == 0) { // the amount of blocks to be added has been reached
                 break;
             }
         }
     }  
 	
-	public void deleteFile(String filename) {
+    public void deleteFile(String filename) {
         INode n = filetoINode.get(filename);
         for (int i =0;i<n.blockCount; i++) {
             deleteBlocks(filename, i);
